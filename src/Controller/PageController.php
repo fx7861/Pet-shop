6e479becb564607dc039c205ab174e7282898012
Corentix78 <acro2sk8@hotmail.fr>
2019-02-14 16:43:58 +0100

    creation de page
    Creation de la page subcategory.html.twig

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -17,9 +17,12 @@ class PageController extends AbstractController
         return $this->render('page/home.html.twig');
     }
 
+    /**
+     * @Route("/subcategory", name="page_subcategory")
+    */
     public function subCategory()
     {
-        return $this->render('page/home.html.twig');
+        return $this->render('page/sub_category.html.twig');
     }
 
 }
