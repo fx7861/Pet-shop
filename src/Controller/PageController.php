96afa22c1f82492a9b73a4a36cd2b642c39dfb4b
picsou20 <xav.riquet@gmx.fr>
2019-02-14 11:14:16 +0100

    Page controller
    création du controller page controller avec la methode home

---
+++ src/Controller/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/" name="page_home")
+     */
+    public function home()
+    {
+        return $this->render('page/home.html.twig');
+    }
+}
