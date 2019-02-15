bb94c4a816643505c3c771b743d25831f3f871b9
Corentix78 <acro2sk8@hotmail.fr>
2019-02-15 11:50:24 +0100

    subCategory
    creation de la route dynamique et de la methode

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Product;
+use App\Entity\SubCategory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -18,11 +21,23 @@ class PageController extends AbstractController
     }
 
     /**
-     * @Route("/subcategory", name="page_subcategory")
-    */
-    public function subCategory()
+     * @Route("/{category<[a-zA-Z0-9\-_\/]+>}/{slug<[a-zA-Z0-9\-_\/]+>}", name="page_subcategory")
+     * @param $slug
+     * @return Response
+     */
+    public function subCategory($slug)
     {
-        return $this->render('page/sub_category.html.twig');
+
+        $subCategory = $this->getDoctrine()
+                        ->getRepository(SubCategory::class)
+                        ->findOneBy(['slug' => $slug]);
+        $products = $subCategory->getProducts();
+
+
+        return $this->render('page/sub_category.html.twig', [
+            'subCategory' => $subCategory,
+            'products' => $products
+        ]);
     }
     
     /**
