56abf8158748204052111c4d314efcf03ddc5012
Issiaka BAMBA <bambaissiaka2002@gmail.com>
2019-02-15 16:02:12 +0100

    category
    Création de la route et de la méthode category

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,23 +15,33 @@ class PageController extends AbstractController
      */
     public function home()
     {
+
         return $this->render('page/home.html.twig');
     }
 
     /**
      * @Route("/subcategory", name="page_subcategory")
-    */
+     */
     public function subCategory()
     {
         return $this->render('page/sub_category.html.twig');
     }
-    
-    /**
-     * @Route("/category")
-     */
-    public function category()
-    {
-        return $this->render('page/category.html.twig');
-    }
 
+    /**
+     * @Route("/{slug<[a-zA-Z0-9\-_\/]+>}", name="page_category")
+     */
+    public function category($slug)
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['slug' => $slug]);
+
+        $subCategories = $category->getSubCategories();
+
+        return $this->render('page/category.html.twig',[
+                 'category' => $category,
+                 'subCategory' => $subCategories
+            ]);
+    }
 }
+
