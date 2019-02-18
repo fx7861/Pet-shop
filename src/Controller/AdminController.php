fefe496e9f36bd6191367a94092873b131a8b738
picsou20 <xav.riquet@gmx.fr>
2019-02-18 09:27:12 +0100

    admincontroler et securitycontroller
    Création du controller AdminController et SecurityController.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+
+class AdminController
+{
+    
+    /**
+     * @Route("/admin/product", name="admin_product")
+     */
+    public function listProduct()
+    {
+
+    }
+}
